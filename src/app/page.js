5dcc469ac4660b1d1630bba458ffb9d1dfda38ed
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-03-21 13:04:06 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,6 @@ export default function Home() {
   return (
     <>
       <Banner />
-
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-4xl font-bold text-center">
-          Rent Your Gadgets, Go Big, Fly Higher
-        </h1>
-        <RentCard />
-      </div>
       <Category />
       <SubBanner />
       <PopularProduct />
