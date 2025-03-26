060ce3a8a605c338bbc1dd76f7a69cc2d376b594
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-03-27 01:17:20 +0600

    updatate the home dashbord gadget page

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,9 +1,9 @@
 import TestData from "./components/TestData";
 
 export default async function Dashboard() {
-  const result = await fetch(process.env.NEXT_PUBLIC_CLIENT_API_CAMERA, {
-    cache: "no-store", // Ensures fresh data on every request
-  }).then(res => res.json());
+  // const result = await fetch(process.env.NEXT_PUBLIC_CLIENT_API_CAMERA, {
+  //   cache: "no-store",
+  // }).then((res) => res.json());
 
   return (
     <div>
