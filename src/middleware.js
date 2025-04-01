67a431637e21678b698d58aa263b5669ade07bfa
arafatrased <rasedmathku@gmail.com>
2025-04-01 22:51:05 +0600

    Protected route are set for dashboard

---
+++ src/middleware.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export const middleware = async (req, res) => {
+
+    const token = await getToken({ req });
+    // const isAuthenticated = !!token;
+
+    const isTokenOk = !!token;
+    const isAdmin = token?.role === 'admin';
+    const isLender = token?.role === 'lender';
+    const isRenter = token?.role === 'renter';
+
+    const isAuthenticated = isTokenOk && (isAdmin || isLender || isRenter);
+    // const isLoginPath = req.nextUrl.pathname.startsWith('/login');
+    // const isRegisterPath = req.nextUrl.pathname.startsWith('/register');
+    const isDashBoardPath = req.nextUrl.pathname.startsWith('/dashboard');
+    if(!isAuthenticated && isDashBoardPath) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+
+
+    const isAdminSpecificPath = req.nextUrl.pathname.startsWith('/dashboard/add-gadget');
+    if(isAdminSpecificPath && !isAuthenticated) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+  // Check if the request is for the homepage
+//   if (req.url === '/') {
+//     // Redirect to the gadgets page
+//     return res.redirect('/gadgets');
+//   }
+
+  // Continue with the request
+  return NextResponse.next();
+}
