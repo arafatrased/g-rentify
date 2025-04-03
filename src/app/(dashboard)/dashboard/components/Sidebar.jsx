13c0f56a9ab6f9a41e7035bdc32564c87e489e5d
arafatrased <rasedmathku@gmail.com>
2025-04-03 17:09:57 +0600

    settings navigation is set

--- src/app/(dashboard)/dashboard/components/Sidebar.jsx
+++ src/app/(dashboard)/dashboard/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { FiHome } from "react-icons/fi";
+import { FiUser, FiHome } from "react-icons/fi";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { TbPlaylistAdd } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
 
 export default function Sidebar() {
   const links = (
@@ -29,6 +30,29 @@ export default function Sidebar() {
       </li>
     </>
   );
+  const userLinks = (
+    <>
+      <li>
+        <Link href={"/dashboard/settings"} className="flex justify-between items-center">
+          <span className="flex items-center gap-1">
+            <FiUser />
+           View Profile
+          </span>
+          <MdKeyboardArrowRight />
+        </Link>
+      </li>
+      <li>
+        <Link href={"/dashboard/profile"} className="flex justify-between items-center">
+          <span className="flex items-center gap-1">
+            <IoSettingsOutline />
+            Settings
+          </span>
+          <MdKeyboardArrowRight />
+        </Link>
+      </li>
+    </>
+  );
+
   return (
     <div className="bg-[#03b00b]  w-[250px] min-h-screen">
       <div className="p-3 sticky top-0">
@@ -40,7 +64,12 @@ export default function Sidebar() {
 
         <div className="w-full h-[1px] bg-green-600 my-2"></div>
         <p className="text-[12px] text-gray-200">Dashboard & App</p>
-        <ul className="text-[#edf5ff] mt-5 space-y-4">{links}</ul>
+        <div className="flex flex-col gap-2 mt-2 justify-between min-h-[80vh]">
+          <ul className="text-[#edf5ff] mt-5 space-y-4">{links}</ul>
+          <div className="flex-1"></div>
+          <ul className="text-[#edf5ff] mt-5 space-y-4">{userLinks}</ul>
+        </div>
+
       </div>
     </div>
   );
